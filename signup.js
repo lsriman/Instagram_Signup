5666fcc3c92db00544f26f084139a4cca7ad90d3
lsriman <lsriman@gmail.com>
2023-12-13 20:08:50 +0530

    new

---
+++ signup.js
@@ -0,0 +1,84 @@
+// function signup(){
+  
+//     mob=document.getElementById('mobno');
+//     fname=document.getElementById('fullname');
+//     uname=document.getElementById('username');
+//     pass=document.getElementById('pass');
+    
+
+// let person={
+//     mobile: mob.value,
+//     fullname: fname.value,
+//     username: uname.value,
+//     password: pass.value,
+// }
+
+// let person1=[mobile= mob.value,fullname= fname.value,username= uname.value,password= pass.value]
+       
+//     console.log(person)
+//     console.log(person1)
+//     deform={mobile,fullname,username,password}=person
+//     alert(`your:-  Mob/Email: "${mobile}", Fullname: "${fullname}", Username: "${username}", Password:"${password}".`);
+    
+// }
+
+
+function signup(){
+     let mob=document.getElementById('mobno');
+     let fname=document.getElementById('fullname');
+     let uname=document.getElementById('username');
+     let pass=document.getElementById('pass');
+
+    if(mob.value.trim() === '' || fname.value.trim() === '' || uname.value.trim() === '' || pass.value.trim() === ''){
+        validate()
+    }
+    else{
+        add()
+    }
+}
+
+function validate(){
+    let mob=document.getElementById('mobno');
+    let fname=document.getElementById('fullname');
+    let uname=document.getElementById('username');
+    let pass=document.getElementById('pass');
+    if(mob.value.trim() === ''){
+        alert('Fill Mob or Email')
+    }
+    else if(fname.value.trim() === ''){
+        alert('Fill Fullname')
+    }
+    else if(uname.value.trim() === ''){
+        alert('Fill Username')
+    }
+    else if(pass.value.trim() === ''){
+        alert('Fill Password')
+    }
+    else{
+        add()
+    }
+}
+
+function add(){
+        let mob=document.getElementById('mobno');
+        let fname=document.getElementById('fullname');
+        let uname=document.getElementById('username');
+        let pass=document.getElementById('pass');
+        
+    
+     let person={
+         mobile: mob.value,
+         fullname: fname.value,
+         username: uname.value,
+         password: pass.value,
+     }
+    
+     let person1=[mobile= mob.value,fullname= fname.value,username= uname.value,password= pass.value]
+           
+         console.log(person)
+         console.log(person1)
+         deform={mobile,fullname,username,password}=person
+         alert(`your:-  Mob/Email: "${mobile}", Fullname: "${fullname}", Username: "${username}", Password:"${password}".`);
+        
+    
+}
